9a9c03f215806ccfa7872a5a7cb7b8f19cf00a75
Onur Demir <onur.de@live.de>
2020-05-26 09:23:51 +0200

    first commit

---
+++ NonSecure/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /Users/windy/STM32CubeIDE/workspace_1.3.0/TrustZone-ToggleLED/Secure/Debug/secure_nsclib.o
+
+LIBS :=
+
